e86202f6e8303538aae502b8554ffa6948f576e8
John-Khalil <johnadelkhalil@gmail.com>
2023-04-17 07:09:44 +0200

    directions callback

--- xtensaPNP/src/components/Controls.jsx
+++ xtensaPNP/src/components/Controls.jsx
@@ -6,7 +6,7 @@ import { CONTROLPANEL_FEEDRATE, CONTROLPANEL_SELECTED_TOOL, CONTROLPANEL_UNIT, u
 
 
 export const ControlPanel=({machineControl})=>{
-  const buttonStyle='bg-teal-400 text-center h-full w-full';
+  const buttonStyle='bg-teal-900 text-center h-full w-full';
   const toolControlStyle='border border-gray-500 h-full w-full';
   const tootChangeStyle='rounded-md bg-red-600 p-1 m-1';
 
@@ -45,14 +45,14 @@ export const ControlPanel=({machineControl})=>{
   const unitUp=()=>{
     unitRef.current.value*=2;
     if(unitRef.current.value<0.1)
-      unitRef.current.value=1;
+      unitRef.current.value=0.1;
     userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
   }
 
   const unitDown=()=>{
     unitRef.current.value/=2;
     if(unitRef.current.value<0.1)
-      unitRef.current.value=1;
+      unitRef.current.value=0.1;
     userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
   }
 
@@ -61,7 +61,7 @@ export const ControlPanel=({machineControl})=>{
     <div className='float-right w-[300px]  '>
       <input type="number" className='w-full bg-gray-800 rounded-lg h-[45px] text-blue-300 text-4xl text-center' ref={unitRef} onChange={()=>{
         if(unitRef.current.value<0.1)
-          unitRef.current.value=1;
+          unitRef.current.value=0.1;
         userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
       }}/>
       
@@ -71,16 +71,16 @@ export const ControlPanel=({machineControl})=>{
 
 
         <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUp}>01</div></div>
-        <div className='row-start-1 col-start-3 row-span-1 col-span-1'><div className={buttonStyle}>02</div></div>
-        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>03</div></div>
+        <div className='row-start-1 col-start-3 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).Y_Positive)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>02</div></div>
+        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).Z_Positive)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>03</div></div>
 
-        <div className='row-start-3 col-start-1 row-span-1 col-span-1'><div className={buttonStyle}>04</div></div>
+        <div className='row-start-3 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).X_Negative)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>04</div></div>
         <div className='row-start-2 col-start-2 row-span-3 col-span-3'><div className={toolControlStyle}>{toolControl}</div></div>
-        <div className='row-start-3 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>06</div></div>
+        <div className='row-start-3 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).X_Positive)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>06</div></div>
 
         <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDown}>07</div></div>
-        <div className='row-start-5 col-start-3 row-span-1 col-span-1'><div className={buttonStyle}>08</div></div>
-        <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>09</div></div>
+        <div className='row-start-5 col-start-3 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).Y_Negative)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>08</div></div>
+        <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>((machineControl||{}).Z_Negative)||(()=>{})({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>09</div></div>
         
       </div>
       <div className='w-full'>
