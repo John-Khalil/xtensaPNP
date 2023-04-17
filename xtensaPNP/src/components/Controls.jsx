77d6d7941511c6327044439d3aeb06a69738599a
John-Khalil <johnadelkhalil@gmail.com>
2023-04-17 02:23:56 +0200

    ControlPanel

--- xtensaPNP/src/components/Controls.jsx
+++ xtensaPNP/src/components/Controls.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
-export const ControlPanel=()=>{
+export const ControlPanel=({machineControl})=>{
   const buttonStyle='bg-teal-400 text-center h-full w-full';
-  const toolControlStyle='bg-teal-400 text-center h-full w-full'
+  const toolControlStyle='border border-gray-500 h-full w-full';
+  const tootChangeStyle='rounded-md bg-red-600 p-1 m-1';
+
+
+  const [toolChangeList,setToolChangeList]=useState([]);
+  const [toolControl,setToolControl]=useState(<>tool control</>);
+
+  useEffect(()=>{
+    const toolChangeListBuffer=[];
+
+    ((machineControl||{}).toolChangeList||[]).forEach((tool,index) => {
+
+
+      toolChangeListBuffer.push(
+        <button className={`float-left ${tootChangeStyle}`} onClick={()=>{
+          ((tool.activate)||(()=>{}))();
+          setToolControl(tool.Control||<>undefined</>);
+        }}>{tool.label||'tool'}</button>
+      );
+    });
+
+    setToolChangeList(toolChangeListBuffer);
+
+  },[]);
+
 
   return(<>
-    <div className='float-right w-[300px] bg-orange-300 '>
+    <div className='float-right w-[300px]  '>
       <input type="number" className='w-full bg-gray-800 rounded-lg h-[45px] text-blue-300 text-4xl text-center' />
-      <div className='grid grid-cols-5 grid-rows-5 gap-2   w-full h-[300px]'>
+      
+      <div className='grid grid-cols-5 grid-rows-5 gap-0.5   w-full h-[300px] my-2'>
 
           {/* <div className='row-start-1 col-start-1 row-end-11 col-end-11'><div className=''></div></div> */}
 
@@ -18,30 +45,19 @@ export const ControlPanel=()=>{
         <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>03</div></div>
 
         <div className='row-start-3 col-start-1 row-span-1 col-span-1'><div className={buttonStyle}>04</div></div>
-        <div className='row-start-2 col-start-2 row-span-3 col-span-3'><div className={toolControlStyle}>05</div></div>
+        <div className='row-start-2 col-start-2 row-span-3 col-span-3'><div className={toolControlStyle}>{toolControl}</div></div>
         <div className='row-start-3 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>06</div></div>
 
         <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle}>07</div></div>
         <div className='row-start-5 col-start-3 row-span-1 col-span-1'><div className={buttonStyle}>08</div></div>
         <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle}>09</div></div>
-
-
-
-        {/* <div className='row-start-2 col-start-1 row-end-4 col-end-3'><div className={buttonStyle}>01</div></div>
-        <div className='row-start-5 col-start-1 row-end-7 col-end-3'><div className={buttonStyle}>04</div></div>
-        <div className='row-start-7 col-start-1 row-end-9 col-end-3'><div className={buttonStyle}>07</div></div>
-
-        <div className='row-start-1 col-start-5 row-end-3 col-end-7'><div className={buttonStyle}>02</div></div>
-        <div className='row-start-3 col-start-3 row-end-9 col-end-9'><div className={toolControlStyle}>05</div></div>
-        <div className='row-start-9 col-start-5 row-end-11 col-end-7'><div className={buttonStyle}>08</div></div>
-
-        <div className='row-start-2 col-start-9 row-end-4 col-end-11'><div className={buttonStyle}>03</div></div>
-        <div className='row-start-5 col-start-9 row-end-7 col-end-11'><div className={buttonStyle}>06</div></div>
-        <div className='row-start-7 col-start-9 row-end-9 col-end-11'><div className={buttonStyle}>09</div></div> */}
-        
         
       </div>
-
+      <span>Feed Rate</span>
+      <input type="range" className='w-full'/>
+      <div className='w-full my-2 overflow-scroll inline'>
+        {toolChangeList}
+      </div>
     </div>
 
   </>);
@@ -52,7 +68,13 @@ export const ControlPanel=()=>{
 export default function Controls() {
   return (
     <>
-      <ControlPanel/>
+      <ControlPanel machineControl={{
+        toolChangeList:[
+          {},{Control:
+            <div>manga</div>
+          },{},{},{},{},{}
+        ]        
+      }}/>
     </>
   )
 }
