01d2253e6fd0adc871d62cc517eaf15d8d729baf
John-Khalil <johnadelkhalil@gmail.com>
2023-04-24 23:06:18 +0200

    strict mode disabled

--- xtensaPNP/src/utils/operators.js
+++ xtensaPNP/src/utils/operators.js
@@ -100,7 +100,6 @@ export const execuatableSetup=()=>{
 
     appLinker.addListener(EXECUATABLE_RETURN,async(data)=>{
         
-        console.log(" ------- event.data >> ",data)
         execuatable.operatorReturn(data);
     });
     
