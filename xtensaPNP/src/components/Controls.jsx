293c9a2e60f555c352418f9eb3571ed0332f3e80
John-Khalil <johnadelkhalil@gmail.com>
2023-04-24 04:30:28 +0200

    ControlPanel update

--- xtensaPNP/src/components/Controls.jsx
+++ xtensaPNP/src/components/Controls.jsx
@@ -3,15 +3,16 @@ import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import execuatable from '../utils/operators';
-import appLinker, { CONTROLPANEL_FEEDRATE, CONTROLPANEL_SELECTED_TOOL, CONTROLPANEL_UNIT, EXECUATABLE_PROCESS, userStorage } from '../utils/utils';
+import appLinker, { CONTROLPANEL_FEEDRATE, CONTROLPANEL_SELECTED_TOOL, CONTROLPANEL_UNIT, CONTROLPANEL_UNITZ, EXECUATABLE_PROCESS, userStorage } from '../utils/utils';
 
 
 export const ControlPanel=({machineControl})=>{
-  const buttonStyle='bg-teal-900 text-center h-full w-full';
+  const buttonStyle='bg-teal-900 text-center h-full w-full select-none p-4 font-bold text-lg rounded-md';
   const toolControlStyle='border border-gray-500 h-full w-full';
   const tootChangeStyle='rounded-md bg-red-600 p-1 m-1';
 
   const unitRef=useRef();
+  const unitZRef=useRef();
   const feedRateRef=useRef();
   const feedRateSliderRef=useRef();
 
@@ -36,6 +37,7 @@ export const ControlPanel=({machineControl})=>{
     setToolChangeList(toolChangeListBuffer);
 
     unitRef.current.value=userStorage.get(CONTROLPANEL_UNIT)||userStorage.set(CONTROLPANEL_UNIT,1);
+    unitZRef.current.value=userStorage.get(CONTROLPANEL_UNITZ)||userStorage.set(CONTROLPANEL_UNITZ,1);
     feedRateRef.current.value=userStorage.get(CONTROLPANEL_FEEDRATE)||userStorage.set(CONTROLPANEL_FEEDRATE,100);
     feedRateSliderRef.current.value=userStorage.get(CONTROLPANEL_FEEDRATE)||userStorage.set(CONTROLPANEL_FEEDRATE,100);
     
@@ -60,27 +62,31 @@ export const ControlPanel=({machineControl})=>{
 
   return(<>
     <div className='float-right w-[300px]  '>
-      <input type="number" className='w-full bg-gray-800 rounded-lg h-[45px] text-blue-300 text-4xl text-center' ref={unitRef} onChange={()=>{
-        if(unitRef.current.value<0.1)
-          unitRef.current.value=0.1;
-        userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
-      }}/>
+
+      <div className='w-full my-2 overflow-scroll'>
+        <div className='min-w-max'>
+          {toolChangeList}
+        </div>
+      </div>
+
       
-      <div className='grid grid-cols-5 grid-rows-5 gap-0.5   w-full h-[300px] my-2'>
+      
+      
+      <div className='grid grid-cols-5 grid-rows-5 gap-1   w-full h-[300px] my-2'>
 
           {/* <div className='row-start-1 col-start-1 row-end-11 col-end-11'><div className=''></div></div> */}
 
 
-        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUp}>01</div></div>
-        <div className='row-start-1 col-start-3 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>02</div></div>
+        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>{}}>01</div></div>
+        <div className='row-start-1 col-start-2 row-span-1 col-span-3'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>02</div></div>
         <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>03</div></div>
 
-        <div className='row-start-3 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>04</div></div>
+        <div className='row-start-2 col-start-1 row-span-3 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>04</div></div>
         <div className='row-start-2 col-start-2 row-span-3 col-span-3'><div className={toolControlStyle}>{toolControl}</div></div>
-        <div className='row-start-3 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>06</div></div>
+        <div className='row-start-2 col-start-5 row-span-3 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>06</div></div>
 
-        <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDown}>07</div></div>
-        <div className='row-start-5 col-start-3 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>08</div></div>
+        <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>{}}>07</div></div>
+        <div className='row-start-5 col-start-2 row-span-1 col-span-3'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>08</div></div>
         <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>09</div></div>
         
       </div>
@@ -99,11 +105,33 @@ export const ControlPanel=({machineControl})=>{
         feedRateRef.current.value=feedRateSliderRef.current.value;
 
       }}/>
-      <div className='w-full my-2 overflow-scroll'>
-        <div className='min-w-max'>
-          {toolChangeList}
+      
+      <span className='float-left'>UNIT-XY</span>
+      <div className='grid grid-cols-5 grid-rows-1 gap-1   w-full h-[60px] my-2'>
+        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDown}>-</div></div>
+        <div className='row-start-1 col-start-2 row-span-1 col-span-3'>
+          <input type="number" className='w-full bg-gray-800 rounded-lg h-full text-blue-300 text-4xl text-center' ref={unitRef} onChange={()=>{
+            if(unitRef.current.value<0.1)
+              unitRef.current.value=0.1;
+            userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
+          }}/>
         </div>
+        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUp}>+</div></div>
       </div>
+      <span className='float-left'>UNIT-Z</span>
+      <div className='grid grid-cols-5 grid-rows-1 gap-1   w-full h-[60px] my-2'>
+        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDown}>-</div></div>
+        <div className='row-start-1 col-start-2 row-span-1 col-span-3'>
+          <input type="number" className='w-full bg-gray-800 rounded-lg h-full text-blue-300 text-4xl text-center' ref={unitZRef} onChange={()=>{
+            if(unitZRef.current.value<0.1)
+              unitZRef.current.value=0.1;
+            userStorage.set(CONTROLPANEL_UNITZ,unitZRef.current.value);
+          }}/>
+        </div>
+        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUp}>+</div></div>
+      </div>
+
+
     </div>
 
   </>);
