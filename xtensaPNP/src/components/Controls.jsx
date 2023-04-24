b09d1280f01ee5600085e94558061075cca4c4b1
John-Khalil <johnadelkhalil@gmail.com>
2023-04-24 04:52:37 +0200

    ControlPanel update

--- xtensaPNP/src/components/Controls.jsx
+++ xtensaPNP/src/components/Controls.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import execuatable from '../utils/operators';
-import appLinker, { CONTROLPANEL_FEEDRATE, CONTROLPANEL_SELECTED_TOOL, CONTROLPANEL_UNIT, CONTROLPANEL_UNITZ, EXECUATABLE_PROCESS, userStorage } from '../utils/utils';
+import appLinker, { CONTROLPANEL_FEEDRATE, CONTROLPANEL_FEEDRATE_MAX, CONTROLPANEL_SELECTED_TOOL, CONTROLPANEL_UNIT, CONTROLPANEL_UNITZ, EXECUATABLE_PROCESS, userStorage } from '../utils/utils';
 
 
 export const ControlPanel=({machineControl})=>{
@@ -40,6 +40,7 @@ export const ControlPanel=({machineControl})=>{
     unitZRef.current.value=userStorage.get(CONTROLPANEL_UNITZ)||userStorage.set(CONTROLPANEL_UNITZ,1);
     feedRateRef.current.value=userStorage.get(CONTROLPANEL_FEEDRATE)||userStorage.set(CONTROLPANEL_FEEDRATE,100);
     feedRateSliderRef.current.value=userStorage.get(CONTROLPANEL_FEEDRATE)||userStorage.set(CONTROLPANEL_FEEDRATE,100);
+    feedRateSliderRef.current.max=userStorage.get(CONTROLPANEL_FEEDRATE_MAX)||userStorage.set(CONTROLPANEL_FEEDRATE,100);
     
     ((((machineControl||{}).toolChangeList||[])[userStorage.get(CONTROLPANEL_SELECTED_TOOL)||userStorage.set(CONTROLPANEL_SELECTED_TOOL,0)].activate)||(()=>{}))();
 
@@ -57,6 +58,20 @@ export const ControlPanel=({machineControl})=>{
     if(unitRef.current.value<0.1)
       unitRef.current.value=0.1;
     userStorage.set(CONTROLPANEL_UNIT,unitRef.current.value);
+  }
+
+  const unitUpZ=()=>{
+    unitZRef.current.value*=2;
+    if(unitZRef.current.value<0.1)
+      unitZRef.current.value=0.1;
+    userStorage.set(CONTROLPANEL_UNITZ,unitZRef.current.value);
+  }
+
+  const unitDownZ=()=>{
+    unitZRef.current.value/=2;
+    if(unitZRef.current.value<0.1)
+      unitZRef.current.value=0.1;
+    userStorage.set(CONTROLPANEL_UNITZ,unitZRef.current.value);
   }
 
 
@@ -77,17 +92,17 @@ export const ControlPanel=({machineControl})=>{
           {/* <div className='row-start-1 col-start-1 row-end-11 col-end-11'><div className=''></div></div> */}
 
 
-        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>{}}>01</div></div>
+        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).unlock)||(()=>{}))()}>01</div></div>
         <div className='row-start-1 col-start-2 row-span-1 col-span-3'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>02</div></div>
-        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>03</div></div>
+        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Positive)||(()=>{}))({unit:unitZRef.current.value,feedRate:feedRateRef.current.value})}>03</div></div>
 
         <div className='row-start-2 col-start-1 row-span-3 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>04</div></div>
         <div className='row-start-2 col-start-2 row-span-3 col-span-3'><div className={toolControlStyle}>{toolControl}</div></div>
         <div className='row-start-2 col-start-5 row-span-3 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).X_Positive)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>06</div></div>
 
-        <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>{}}>07</div></div>
+        <div className='row-start-5 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).stop)||(()=>{}))()}>07</div></div>
         <div className='row-start-5 col-start-2 row-span-1 col-span-3'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Y_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>08</div></div>
-        <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Negative)||(()=>{}))({unit:unitRef.current.value,feedRate:feedRateRef.current.value})}>09</div></div>
+        <div className='row-start-5 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={()=>(((machineControl||{}).Z_Negative)||(()=>{}))({unit:unitZRef.current.value,feedRate:feedRateRef.current.value})}>09</div></div>
         
       </div>
       <div className='w-full'>
@@ -96,6 +111,7 @@ export const ControlPanel=({machineControl})=>{
           if(feedRateRef.current.value<1)
             feedRateRef.current.value=1;
           userStorage.set(CONTROLPANEL_FEEDRATE,feedRateRef.current.value);
+          userStorage.set(CONTROLPANEL_FEEDRATE_MAX,feedRateRef.current.value);
           feedRateSliderRef.current.max=feedRateRef.current.value;
           feedRateSliderRef.current.value=feedRateRef.current.value;
         }}/>
@@ -120,7 +136,7 @@ export const ControlPanel=({machineControl})=>{
       </div>
       <span className='float-left'>UNIT-Z</span>
       <div className='grid grid-cols-5 grid-rows-1 gap-1   w-full h-[60px] my-2'>
-        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDown}>-</div></div>
+        <div className='row-start-1 col-start-1 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitDownZ}>-</div></div>
         <div className='row-start-1 col-start-2 row-span-1 col-span-3'>
           <input type="number" className='w-full bg-gray-800 rounded-lg h-full text-blue-300 text-4xl text-center' ref={unitZRef} onChange={()=>{
             if(unitZRef.current.value<0.1)
@@ -128,7 +144,7 @@ export const ControlPanel=({machineControl})=>{
             userStorage.set(CONTROLPANEL_UNITZ,unitZRef.current.value);
           }}/>
         </div>
-        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUp}>+</div></div>
+        <div className='row-start-1 col-start-5 row-span-1 col-span-1'><div className={buttonStyle} onClick={unitUpZ}>+</div></div>
       </div>
 
 
@@ -184,26 +200,26 @@ export default function Controls() {
     //   }
     // ]})
 
-    setTimeout(() => {
-      appLinker.send(EXECUATABLE_PROCESS,{
-        operator:execuatable.EXECUATABLE_THREAD,
-        execuatableList:[
-          {
-            operator:execuatable.EXECUATABLE_MOTION_CONTROLLER,
-          },
-          {
-            operator:execuatable.EXECUATABLE_OUTPUT_DEVICE,
-          },
-          {
-            operator:execuatable.EXECUATABLE_INPUT_DEVICE,
-            customKey:'this is a text',
-            inputDevice:(inputDeviceData=>{
-              console.log("inputDeviceData >> ",inputDeviceData);
-            })
-          }
-        ]
-      })
-    }, 2000);
+    // setTimeout(() => {
+    //   appLinker.send(EXECUATABLE_PROCESS,{
+    //     operator:execuatable.EXECUATABLE_THREAD,
+    //     execuatableList:[
+    //       {
+    //         operator:execuatable.EXECUATABLE_MOTION_CONTROLLER,
+    //       },
+    //       {
+    //         operator:execuatable.EXECUATABLE_OUTPUT_DEVICE,
+    //       },
+    //       {
+    //         operator:execuatable.EXECUATABLE_INPUT_DEVICE,
+    //         customKey:'this is a text',
+    //         inputDevice:(inputDeviceData=>{
+    //           console.log("inputDeviceData >> ",inputDeviceData);
+    //         })
+    //       }
+    //     ]
+    //   })
+    // }, 2000);
 
     
 
@@ -228,6 +244,12 @@ export default function Controls() {
         },
         Z_Negative:(data)=>{
           console.log("Z_Negative >> ",data);
+        },
+        unlock:()=>{
+          console.log("unlock");
+        },
+        stop:()=>{
+          console.log("stop");
         },
         toolChangeList:[
           {
