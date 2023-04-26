014a4df286f869c14e441224ee047c9a3c11555b
John-Khalil <johnadelkhalil@gmail.com>
2023-04-26 04:02:38 +0200

    AppSettings

--- xtensaPNP/src/components/AppSettings.jsx
+++ xtensaPNP/src/components/AppSettings.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import { useRef } from 'react';
-import { userStorage } from '../../../backend/utils';
+import { userStorage,WEBSOCKET_REMOTE_HOST,WEBSOCKET_REMOTE_PORT,WEBSOCKET_REMOTE_PATH,CAMERA_CONFIG,MOTIONCONTROLLER_IP,MOTIONCONTROLLER_PORT,MOTIONCONTROLLER_PATH} from '../utils/utils';
+
 
 export const SettingsFeild=({inputFeild})=>{
   const formInputStyle='w-full bg-gray-700 rounded-lg text-blue-300 text-xl text-left p-0.5 float-right';
-  const defaultConfig='';
+  const defaultConfig=(inputFeild||{}).defaultConfig||'';
 
   const inputRef=useRef();
   useEffect(()=>{
-    // inputRef.current.value=userStorage.get((inputFeild||{}).label||'settings-label')||userStorage.set((inputFeild||{}).label||'settings-label',defaultConfig);
+    inputRef.current.value=userStorage.get((inputFeild||{}).label||'settings-label')||userStorage.set((inputFeild||{}).label||'settings-label',defaultConfig);
   },[])
   return(
     <>
@@ -23,7 +24,7 @@ export const SettingsFeild=({inputFeild})=>{
         </div>
         <div className='float-right w-[20%] m-1 p-1'>
           <button className='rounded-md bg-green-600 p-1 font-bold' onClick={()=>{
-
+            userStorage.set((inputFeild||{}).label||'settings-label',inputRef.current.value);
           }}>Submit</button>
         </div>
       </div>
@@ -36,7 +37,28 @@ export default function AppSettings() {
     <>
       <div className='w-[50%]'>
         
-        <SettingsFeild/>
+        <SettingsFeild inputFeild={{
+          label:WEBSOCKET_REMOTE_HOST
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:WEBSOCKET_REMOTE_PORT,
+          type:'number'
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:WEBSOCKET_REMOTE_PATH
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:CAMERA_CONFIG
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:MOTIONCONTROLLER_IP
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:MOTIONCONTROLLER_PORT
+        }}/>
+        <SettingsFeild inputFeild={{
+          label:MOTIONCONTROLLER_PATH
+        }}/>
 
       </div>
     </>
